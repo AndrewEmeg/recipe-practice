3d12ea2be935fdbfd5ee0818e04c684801e74939
Andrew Emeghebo <andrewemeghebo2023@gmail.com>
2024-07-19 12:45:37 -0400

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// import { useState } from "react";
+import "./App.css";
+// import firebase from "./FirebaseConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Recipes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD4dEVuJzT-_TPWipkKFLLitaoQ0NUI7-U",
+//   authDomain: "recipe-practice-con.firebaseapp.com",
+//   projectId: "recipe-practice-con",
+//   storageBucket: "recipe-practice-con.appspot.com",
+//   messagingSenderId: "1036747445824",
+//   appId: "1:1036747445824:web:013e7e5e12af045f28f1a5",
+//   measurementId: "G-4GBKD3P1ZM"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
